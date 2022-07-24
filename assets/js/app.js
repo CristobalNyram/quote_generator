121d7bdb91856a0c5c7a2d5d9f9ae853f2e5087f
cristobal nyram <crisco.nyram@gmail.com>
2022-07-24 16:11:30 -0500

    I have added funcionality

---
+++ assets/js/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+//variable
+let btn =document.getElementById('new-quote');
+let quote =document.querySelector('.quote');
+let person =document.querySelector('.person');
+
+const quotes =[
+    {
+        quote:' "Los mejores emprendedores tienen dos cosas en común: nunca se rinden y nunca paran de aprender."',
+        person:'John Freddy Vega',
+    },
+    {
+        quote:' "El mejor momento para sembrar un árbol, fue veinte años antes". ',
+        person:'Anónimo',
+
+    },
+    {
+        quote:'"Nunca pares de aprender"',
+        person:'Platzi'
+    },
+    {
+        quote:'"La vida que quieres está después del miedo"',
+        person:'Diego dreyfus'
+    }
+];
+
+btn.addEventListener('click',()=>{
+    event.preventDefault();
+
+    let random =Math.floor(Math.random() * quotes.length);
+
+    quote.innerText = quotes[random].quote;
+    person.innerText = quotes[random].person;
+
+});
